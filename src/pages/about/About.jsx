c55d285fd291f125ea8bd5e2f6752b63870c11ce
Yash Sakre <yashsakre940@gmail.com>
2024-06-16 19:42:12 +0530

    new project added:

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -34,11 +34,11 @@ const About = () => {
     );
   }
   return (
-    <div className="xl:min-h-screen flex flex-col" id="about">
+    <div className="flex flex-col xl:min-h-[80vh]" id="about">
       <div className="text-[3rem]">About Me.</div>
       <hr />  
-      <div className=" md:flex flex-col py-4  items-center  ">
-        <div className="w-full justify-center md:text-2xl font-medium">
+      <div className="flex-col items-center py-4 md:flex">
+        <div className="justify-center w-full font-medium md:text-2xl">
           <span className="ml-[50%]">
             I bring not just a passion for frontend development but a unique
             blend 
@@ -47,9 +47,9 @@ const About = () => {
           diverse experiences, infusing a distinctive perspective into my work
           and fostering a holistic approach to problem-solving.
         </div>
-        <div className="  flex items-center w-full mt-4    ">
-          <div className="md:flex  w-full gap-10">
-            <div className="  md:flex items-center  justify-center">
+        <div className="flex items-center w-full mt-4 ">
+          <div className="w-full gap-10 md:flex">
+            <div className="items-center justify-center md:flex">
               <div className="flex justify-center image-3d">
                 <img
                   className=" w-[50%]  md:w-[40%] md:mr-[30%] mb-4 md:mb-0 rounded-full border-2 object-contain about-card"
@@ -63,14 +63,14 @@ const About = () => {
                 a national-level Cybersecurity Hackathon.
               </div>
             </div>
-            <div className="text-sm font-medium mt-10  w-full">
+            <div className="w-full mt-10 text-sm font-medium">
               Thriving in collaborative environments, I bring a strong work
               ethic, problem-solving prowess, and a commitment to delivering
               visually captivating code for outstanding user experiences.
               <div className="">
                 <button
                   onClick={resume}
-                  className="btn flex justify-center items-center gap-1 my-3 py-2 px-4 hover border-2"
+                  className="flex items-center justify-center gap-1 px-4 py-2 my-3 border-2 btn hover"
                 >
                   <AiOutlineLink />
                   <span>Resume</span>
@@ -84,10 +84,10 @@ const About = () => {
       <div className="flex flex-col">
         <div className="text-[3rem]">Skills.</div>
         <hr />
-        <div className="flex flex-col  flex-wrap mt-5">
-          <div className="flex flex-row flex-wrap  md:gap-5 gap-2 items-center justify-center ">
+        <div className="flex flex-col flex-wrap mt-5">
+          <div className="flex flex-row flex-wrap items-center justify-center gap-2 md:gap-5 ">
             {skills.map((skill, index) => (
-              <div key={index} className="flex justify-center items-center border-2 py-2 px-4 rounded-full gap-2 hover">
+              <div key={index} className="flex items-center justify-center gap-2 px-4 py-2 border-2 rounded-full hover">
                 <img
                   className="object-cover w-5"
                   src={imageMap[skill.imagePath]}
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,5 +1,6 @@
 import test from "../../assets/finecode.png";
 import Flexfit from "../../assets/Flexfit.png";
+import TypeArena from "../../assets/TypeArena.png";
 import opinhacks from "../../assets/Opinhacks.png";
 import projects from "../../components/projects.json";
 import { AiOutlineLink } from "react-icons/ai";
@@ -8,6 +9,7 @@ const Projects = () => {
     "../assests/finecode.png": test,
     "../assests/Flexfit.png": Flexfit,
     "../assests/Opinhacks.png": opinhacks,
+    "../assests/TypeArena.png": TypeArena
 
     // Map more image paths to imported images
   };
@@ -15,9 +17,9 @@ const Projects = () => {
     <div className="min-h-screen " id="projects">
       <div className="text-[3rem]"> Projects.</div>
       <hr />
-      <div className=" flex flex-col w-full gap-20 mt-10 mb-10 ">
+      <div className="flex flex-col w-full gap-20 mt-10 mb-10 ">
         {projects.map((items, index) => (
-          <div key={index} className="lg:flex gap-10 ">
+          <div key={index} className="gap-10 lg:flex ">
             <div className="image-3d">
               <img
                 src={imageMap[items.imagePath]}
@@ -26,22 +28,22 @@ const Projects = () => {
                 
               />
             </div>
-            <div className="lg:mt-0 mt-3">
+            <div className="mt-3 lg:mt-0">
               <div className="text-4xl font-medium underline">
                 {items.ProjName}
               </div>
-              <div className="text-2xl mt-2 mb-3">{items.Projtitle}</div>
+              <div className="mt-2 mb-3 text-2xl">{items.Projtitle}</div>
               <div className="mb-2">{items.ProjDesc}</div>
               <hr />
               <div className="mt-2 ">
                 Tech Stack - <span className="font-medium">{items.tech}</span>
               </div>
               <div className="flex items-center mt-4">
-                <p className=" flex gap-3">
+                <p className="flex gap-3 ">
                   <a
                     href={items.Github}
                     target="__blank"
-                    className="py-1 px-4 border-2 hover flex justify-center items-center gap-1"
+                    className="flex items-center justify-center gap-1 px-4 py-1 border-2 hover"
                   >
                     <AiOutlineLink /> Github
                   </a>
@@ -49,7 +51,7 @@ const Projects = () => {
                     <a
                       href={items.Live}
                       target="__blank"
-                      className="py-1 px-4 border-2   hover flex justify-center items-center gap-1"
+                      className="flex items-center justify-center gap-1 px-4 py-1 border-2 hover"
                     >
                       <AiOutlineLink /> Live
                     </a>
